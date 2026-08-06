8cb54f752925f0e13462b7e5b362b8dcc849ff5b
Ninh2577 <hoangninh2577@gmail.com>
2026-08-06 08:02:58 +0700

    Fix lỗi ảnh đại diện

--- image.php
+++ image.php
@@ -1,11 +1,10 @@
 <?php
-// Script proxy hình ảnh động dành cho Localhost (XAMPP)
-if (isset($_GET['id']) && isset($_GET['region']) && isset($_GET['env'])) {
-    $region = preg_replace('/[^a-zA-Z0-9\-]/', '', $_GET['region']);
-    $env = preg_replace('/[^a-zA-Z0-9]/', '', $_GET['env']);
-    $id = preg_replace('/[^a-zA-Z0-9]/', '', $_GET['id']);
+// Script proxy hình ảnh tổng quát dành cho Localhost (XAMPP)
+if (isset($_GET['host']) && isset($_GET['path'])) {
+    $host = preg_replace('/[^a-zA-Z0-9\.\-]/', '', $_GET['host']);
+    $path = $_GET['path'];
     
-    $url = "https://{$region}.graphassets.com/{$env}/{$id}";
+    $url = "https://{$host}/{$path}";
     
     $headers = @get_headers($url, 1);
     $contentType = isset($headers['Content-Type']) ? $headers['Content-Type'] : (isset($headers['content-type']) ? $headers['content-type'] : 'image/jpeg');
